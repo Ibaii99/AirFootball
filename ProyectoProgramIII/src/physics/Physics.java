ab9c632a5e9051643b262d65941affca218c01d6
ibai <ibai@ubuntu>
2018-10-08 23:43:44 -0700

    Creacion de fisicas

--- ProyectoProgramIII/src/physics/Physics.java
+++ ProyectoProgramIII/src/physics/Physics.java
@@ -1,5 +1,17 @@
 package physics;
 
 public class Physics {
-
+	
+	public static double GRAVEDAD = 9800.0;  // Pixels por segundo cuadrado
+	
+	public static double calcVelocidad( double energia, double masa ) {
+		return Math.sqrt( 2.0 * energia / masa );
+	}
+	
+	
+	
+	
+	
+	
+	
 }
