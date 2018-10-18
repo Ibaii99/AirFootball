4243b9b0c9c56b380acd0bb37c5f8691aadcb947
ibai <ibai@ubuntu>
2018-10-18 13:42:52 -0700

    1.1

--- ProyectoProgramIII/src/menuPrincipal/Liga.java
+++ ProyectoProgramIII/src/menuPrincipal/Liga.java
@@ -1,5 +1,15 @@
 package menuPrincipal;
 
+import java.util.ArrayList;
+
+import entidades.Equipo;
+import entidades.Partidos;
+
 public class Liga {
+	
+	private ArrayList<Equipo> equiposEnLaLiga;
+	private ArrayList<Partidos> partidosPendientes;
+	private ArrayList<Partidos> partidosJugados;
+	
 
 }
