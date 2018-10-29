a6a18f84f64c5ab087bd55d1c097df701c2f5135
ibai <ibai@ubuntu>
2018-10-29 11:39:46 +0100

    Probando base de datos

---
+++ ProyectoProgramIII/src/entidades/BaseDeDatos.java
@@ -0,0 +1,9 @@
+package entidades;
+import java.sql.*;
+import sqlite;
+public class BaseDeDatos {
+	Connection conn: null;
+	Statement stmt = conn.CreateStatement();
+	// tres cosas: connection statement y lo que devuelven las query-s
+}
+
