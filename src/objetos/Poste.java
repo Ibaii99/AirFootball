55611af96bc500476be737bc1dc59ae6a2d3e0c2
ibai <ibai@ubuntu>
2018-11-07 20:07:09 +0100

    1.1 añadiendo metodos que hay que modificar

--- src/objetos/Poste.java
+++ src/objetos/Poste.java
@@ -1,9 +1,64 @@
 package objetos;
 
+import java.awt.Color;
+import java.awt.Point;
+
+import ventanas.ventanaPartido;
+
 /**
  * @author ibai
  *
  */
-public class Poste {
+public class Poste extends Objetos{
+
+
+
+	@Override
+	public double getVolumen() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void borra(ventanaPartido v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void corrigeChoqueInferior(ventanaPartido v, boolean dibujar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void corrigeChoqueLateral(ventanaPartido v, boolean izquierda, boolean dibujar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int chocaConBorde(ventanaPartido v) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Point chocaConObjeto(Objetos objeto2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contieneA(Point punto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
