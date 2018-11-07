f10f825a11213d5dc70c24d94123ac454c2627cf
DonBusto <jorge.elbusto@opendeusto.es>
2018-11-07 20:23:04 +0100

    arreglando resizeo de componentes (in progress)

--- src/ventanas/MenuAmistoso.java
+++ src/ventanas/MenuAmistoso.java
@@ -15,6 +15,7 @@ import javax.swing.JPanel;
 import com.sun.prism.paint.Color;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.BorderLayout;
 
 public class MenuAmistoso extends JFrame {
 	/**
@@ -38,16 +39,15 @@ public class MenuAmistoso extends JFrame {
 	
 
 	public MenuAmistoso(int anchura, int altura) {
-		setSize(anchura, altura);
-		getContentPane().setLayout(null);
+		setSize(626, 458);
 		ImageIcon imageIcon = new ImageIcon(MenuAmistoso.class.getResource("/iconos/stadiumAmistoso.png"));
 		Image image = imageIcon.getImage();
 		
 		Image newimg = image.getScaledInstance(anchura, altura,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
 		ImageIcon icRsz = new ImageIcon(newimg); 
+		getContentPane().setLayout(new BorderLayout(0, 0));
 		
 		final JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 612, 413);
 		getContentPane().add(panel);
 		panel.setLayout(null);
 		
@@ -55,8 +55,8 @@ public class MenuAmistoso extends JFrame {
 		comboBox.setBounds(160, 223, 31, 22);
 		panel.add(comboBox);
 		
-		JButton btnIniciarAmistoso = new JButton("Iniciar amistoso");
-		btnIniciarAmistoso.setBounds(207, 271, 170, 25);
+		final JButton btnIniciarAmistoso = new JButton("Iniciar amistoso");
+		btnIniciarAmistoso.setBounds(207, 271, 159, 27);
 		panel.add(btnIniciarAmistoso);
 		btnIniciarAmistoso.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -66,8 +66,8 @@ public class MenuAmistoso extends JFrame {
 		});
 		btnIniciarAmistoso.setFont(new Font("Arial Black", Font.PLAIN, 13));
 		
-		JLabel lblVisitante = new JLabel("Visitante");
-		lblVisitante.setBounds(356, 203, 65, 16);
+		final JLabel lblVisitante = new JLabel("Visitante");
+		lblVisitante.setBounds(356, 203, 65, 19);
 		panel.add(lblVisitante);
 		lblVisitante.setFont(new Font("Arial Black", Font.PLAIN, 13));
 		
@@ -75,8 +75,8 @@ public class MenuAmistoso extends JFrame {
 		comboBox_1.setBounds(375, 224, 31, 22);
 		panel.add(comboBox_1);
 		
-		JLabel lblLocal = new JLabel("Local");
-		lblLocal.setBounds(155, 204, 42, 16);
+		final JLabel lblLocal = new JLabel("Local");
+		lblLocal.setBounds(155, 204, 40, 19);
 		panel.add(lblLocal);
 		lblLocal.setFont(new Font("Arial Black", Font.PLAIN, 13));
 		
@@ -100,6 +100,17 @@ public class MenuAmistoso extends JFrame {
 				lblFondo.setBounds(0, 0, getWidth(), getHeight());
 				getContentPane().revalidate();
 				getContentPane().setSize(getSize());
+				int nuevaFuente = (13*getWidth()/626);
+				int nuevaAnchura =  (159*getWidth()/626);
+				int nuevaAltura =  (20*getWidth()/626);
+				int nuevaAnchuraBtn =  (40*getWidth()/626);
+				int nuevaAlturaBtn =  (30*getWidth()/626);
+				lblLocal.setFont(new Font("Arial Black", Font.PLAIN, nuevaFuente));
+				lblVisitante.setFont(new Font("Arial Black", Font.PLAIN, nuevaFuente));
+				lblLocal.setBounds(155, 204, 40, 19);
+				btnIniciarAmistoso.setBounds((int) Math.round(((getWidth()/2)-(getWidth()*0.15))), (int) Math.round(((getHeight()/2)+(getHeight()*0.2))), 159*getWidth()/626, 27*getHeight()/626);
+				btnIniciarAmistoso.setFont(new Font("Arial Black", Font.PLAIN, nuevaFuente));
+				revalidate();
 			}
 		});
 	}
