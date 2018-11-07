bb353254aafe5956780459eda7ff442c9eb7748d
ibai <ibai@ubuntu>
2018-11-07 18:39:09 +0100

    1.1 Base de datos con metodo crear tabla hecho

--- src/entidades/BaseDeDatos.java
+++ src/entidades/BaseDeDatos.java
@@ -6,7 +6,7 @@ import java.util.logging.Logger;
 public class BaseDeDatos {
 	private static Logger logger = Logger.getLogger( "MiniPracticaBD" );
 	private static Connection con;
-	private static Statement usuario;
+	private static Statement jugadores;
 	private static Statement liga;
 	private static Statement equipo;
 	private static ResultSet rs;
@@ -18,15 +18,26 @@ public class BaseDeDatos {
 	con = DriverManager.getConnection( "jdbc:sqlite:test.db" );
 	equipo = con.createStatement();
 	liga = con.createStatement();
-	usuario = con.createStatement();
-	}
-	catch(Exception e) {
+	jugadores = con.createStatement();
 		try {
 			comando = "CREATE TABLE \"Equipos\" ( `Siglas` TEXT NOT NULL, `Nombre` TEXT NOT NULL, `Puntos` INTEGER, `Goles Encajados Totales` INTEGER, `Goles Encajados Local` INTEGER, `Goles Encajados Visitante` INTEGER, `Goles A Favor Totales` INTEGER, `Goles A Favor Local` INTEGER, `Goles A Favor Visitante` INTEGER, `Derrotas Totales` INTEGER, `Derrotas Local` INTEGER, `Derrotas Visitante` INTEGER, `Victorias Totales` INTEGER, `Victorias Local` INTEGER, `Victorias Visitante` INTEGER, `Empates Totales` INTEGER, `Empates Local` INTEGER, `Empates Visitante` INTEGER, `Color` TEXT, `Icono` TEXT, PRIMARY KEY(`Siglas`) )";
 			logger.log( Level.INFO, "BD: " + comando );
 			equipo.executeUpdate( comando );
 		} catch (SQLException i) {} // Se lanza si la tabla ya existe - no hay problema
-	}
+		
+		try {
+			comando = "CREATE TABLE \"Liga\" ( `CodLiga` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT )";
+			logger.log( Level.INFO, "BD: " + comando );
+			liga.executeUpdate( comando );
+		} catch (SQLException i) {} // Se lanza si la tabla ya existe - no hay problema
+		
+		try {
+			comando = "CREATE TABLE \"Jugadores\" ( `Nombre` TEXT NOT NULL, `Password` TEXT NOT NULL, PRIMARY KEY(`Nombre`) )";
+			logger.log( Level.INFO, "BD: " + comando );
+			jugadores.executeUpdate( comando );
+		} catch (SQLException i) {} // Se lanza si la tabla ya existe - no hay problema
+	
+	}catch(Exception e) {}
 }
 }
 
