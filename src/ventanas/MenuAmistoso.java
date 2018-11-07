1f4bbf8c592815ab8311a30735459ed73903d4eb
DonBusto <jorge.elbusto@opendeusto.es>
2018-11-07 21:36:58 +0100

    Resizeo de los escudos y componentes del menu hechos

--- src/ventanas/MenuAmistoso.java
+++ src/ventanas/MenuAmistoso.java
@@ -86,7 +86,28 @@ public class MenuAmistoso extends JFrame {
 		panel.add(lblFondo);
 		
 			lblFondo.setIcon(icRsz);
+			
+			final JLabel lblEqL = new JLabel("");
+			lblEqL.setBounds(130, 96, 100, 100);
+			
+			panel.add(lblEqL);
+			
+			final JLabel lblEqV = new JLabel("");
+			lblEqV.setBounds(334, 96, 100, 100);
+			panel.add(lblEqV);
 		
+			ImageIcon imageIconL = new ImageIcon(Inicio.class.getResource("/iconos/equipos/ala.png"));
+			Image imagenResizL = imageIconL.getImage();
+			Image iResizeoL = imagenResizL.getScaledInstance((int) Math.round(100*getWidth()/626), (int) Math.round(100*getWidth()/626),  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+			ImageIcon iiResizeoL = new ImageIcon(iResizeoL); 
+			lblEqL.setIcon(iiResizeoL);
+			
+			ImageIcon imageIconV = new ImageIcon(Inicio.class.getResource("/iconos/equipos/rso.png"));
+			Image imagenResizV = imageIconV.getImage();
+			Image iResizeoV = imagenResizL.getScaledInstance((int) Math.round(100*getWidth()/626), (int) Math.round(100*getWidth()/626),  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+			ImageIcon iiResizeoV = new ImageIcon(iResizeoV); 
+			lblEqV.setIcon(iiResizeoV);
+			
 		addComponentListener(new ComponentAdapter() {
 			@Override
 			public void componentResized(ComponentEvent arg0) {
@@ -97,6 +118,7 @@ public class MenuAmistoso extends JFrame {
 				ImageIcon iiResizeo2 = new ImageIcon(iResizeo); 
 				panel.setSize(getSize());
 				lblFondo.setIcon(iiResizeo2);
+				
 				lblFondo.setBounds(0, 0, getWidth(), getHeight());
 				getContentPane().revalidate();
 				getContentPane().setSize(getSize());
@@ -107,15 +129,31 @@ public class MenuAmistoso extends JFrame {
 				int nuevaAlturaBtn =  (30*getWidth()/626);
 				int nuevaAnchuraCb =  (150*getWidth()/626);
 				int nuevaAlturaCb =  (22*getWidth()/626);
+				int ladoIconoH = (int) Math.round(100*getWidth()/626);
+				int ladoIconoV = (int) Math.round(100*getHeight()*1.36/626);
 				lblLocal.setFont(new Font("Arial Black", Font.PLAIN, nuevaFuente));
 				lblVisitante.setFont(new Font("Arial Black", Font.PLAIN, nuevaFuente));
-				
 				cbVisitante.setBounds((int) Math.round(((getWidth()/2)+(getWidth()*0.01))), (int) Math.round(((getHeight()/2)-(getHeight()*0.01))), nuevaAnchuraCb, nuevaAlturaCb);
 				lblVisitante.setBounds((int) Math.round(((getWidth()/2)+(getWidth()*0.05))), (int) Math.round(((getHeight()/2)-(getHeight()*0.075))), nuevaAnchura, nuevaAltura);
 				cbLocal.setBounds((int) Math.round(((getWidth()/2)-(getWidth()*0.31))), (int) Math.round(((getHeight()/2)-(getHeight()*0.01))), nuevaAnchuraCb, nuevaAlturaCb);
 				lblLocal.setBounds((int) Math.round(((getWidth()/2)-(getWidth()*0.25))), (int) Math.round(((getHeight()/2)-(getHeight()*0.075))), nuevaAnchura, nuevaAltura);
 				btnIniciarAmistoso.setBounds((int) Math.round(((getWidth()/2)-(getWidth()*0.15))), (int) Math.round(((getHeight()/2)+(getHeight()*0.2))), nuevaAnchuraBtn, nuevaAlturaBtn);
 				btnIniciarAmistoso.setFont(new Font("Arial Black", Font.PLAIN, nuevaFuente));
+				
+				ImageIcon imageIconL = new ImageIcon(Inicio.class.getResource("/iconos/equipos/ala.png"));
+				Image imagenResizL = imageIconL.getImage();
+				Image iResizeoL = imagenResizL.getScaledInstance(ladoIconoH, ladoIconoV,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+				ImageIcon iiResizeoL = new ImageIcon(iResizeoL); 
+				lblEqL.setIcon(iiResizeoL);
+				lblEqL.setBounds((int) Math.round(((getWidth()/2)-(getWidth()*0.29))), (int) Math.round(((getHeight()/2)-(getHeight()*0.25))), ladoIconoH, ladoIconoV);
+				
+				ImageIcon imageIconV = new ImageIcon(Inicio.class.getResource("/iconos/equipos/rso.png"));
+				Image imagenResizV = imageIconV.getImage();
+				Image iResizeoV = imagenResizV.getScaledInstance(ladoIconoH, ladoIconoV,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+				ImageIcon iiResizeoV = new ImageIcon(iResizeoV); 
+				lblEqV.setIcon(iiResizeoV);
+				lblEqV.setBounds((int) Math.round(((getWidth()/2)+(getWidth()*0.02))), (int) Math.round(((getHeight()/2)-(getHeight()*0.25))), ladoIconoH, ladoIconoV);
+				
 				revalidate();
 			}
 		});
