0e23f4b098817cd02cbb87e577ba4e292df1991e
ibai <ibai@ubuntu>
2018-11-11 21:43:29 +0100

    Creando objetos basicos de juego en el main

--- src/main/Juego.java
+++ src/main/Juego.java
@@ -4,11 +4,13 @@ import objetos.Poste;
 import ventanas.ventanaPartido;
 
 public class Juego {
+	
 	public ventanaPartido v = new ventanaPartido();
-	public Poste posteArribaIzquierda = new Poste(v);
-	public Poste posteArribaDerecha;
-	public Poste posteAbajoIzquierda;
-	public Poste posteAbajoDerecha;
+	public Poste posteArribaIzquierda = new Poste(v, true, true);
+	public Poste posteAbajoIzquierda = new Poste(v, true, false);
+	
+	public Poste posteArribaDerecha = new Poste(v, false, true);
+	public Poste posteAbajoDerecha = new Poste(v, false, false);
 	
 	
 	public static void main(String[] args) {
