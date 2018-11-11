10b9ba9851470534261591aebb68cc7618fff2fc
ibai <ibai@ubuntu>
2018-11-11 21:41:24 +0100

    Clase poste creada

--- src/main/Juego.java
+++ src/main/Juego.java
@@ -1,6 +1,19 @@
 package main;
 
+import objetos.Poste;
+import ventanas.ventanaPartido;
+
 public class Juego {
+	public ventanaPartido v = new ventanaPartido();
+	public Poste posteArribaIzquierda = new Poste(v);
+	public Poste posteArribaDerecha;
+	public Poste posteAbajoIzquierda;
+	public Poste posteAbajoDerecha;
+	
+	
+	public static void main(String[] args) {
+		
+	}
 	
 	
 
--- src/objetos/Poste.java
+++ src/objetos/Poste.java
@@ -10,23 +10,35 @@ import ventanas.ventanaPartido;
  *
  */
 public class Poste extends Objetos{
-	double ANCHURA_POSTE = 25;
-	double MASA_POSTE = 99999;
-	double ANCHURA_PORTERIA = 120;
+	private static double RADIO_POSTE = 25;
+	private static double MASA_POSTE = 99999;
+	private static double ANCHURA_PORTERIA = 120;
 	
-	public Poste(ventanaPartido v, boolean esPaloDeLaDerecha ) {
-		if(esPaloDeLaDerecha)super(0, calcularPosicionDelPalo(v, ANCHURA_PORTERIA), Color.WHITE, false, "Palo izquierda", ANCHURA_POSTE, MASA_POSTE, null);
+	
+	/** Metodo constructor de un poste, 
+	 *  Si es poste de arriba te lo situa en : La mitad del campo + la anchura de la porteria
+	 *  si es poste de abajo te lo situa en  : La mitad del campo - la anchura de la porteria
+	 * @param v					Ventana de juego
+	 * @param esPosteIzquierda	Booleano para colocar en el eje X el poste segun en que parte este		
+	 * @param esPosteArriba		Booleano para colocar en el eje y el poste segun en que parte este
+	 */
+	public Poste(ventanaPartido v, boolean esPosteIzquierda, boolean esPosteArriba) {
+		super(posicionPoste(v, esPosteIzquierda), ( (v.getAlturaCampo()/2)  + alturaPoste(esPosteArriba)), Color.WHITE, false, "Palo izquierda", RADIO_POSTE, MASA_POSTE, null);
 	}
 
-	/** Metodo para calcular la posicion de un palo de la porteria
-	 * @param v					Ventana donde se van a colocar
-	 * @param anchuraPorteria	Anchura que va a tener la porteria
-	 * @return					devuelve la posicion y del palo
-	 */
-	public double calcularPosicionDelPalo(ventanaPartido v, double anchuraPorteria) {
-		double mitadDeLaPorteria = (v.getAlturaCampo())/2;
-		return(mitadDeLaPorteria + anchuraPorteria);
+
+	private static double posicionPoste(ventanaPartido v,boolean esPosteIzquierda) {
+		double posicion = v.getAnchuraCampo() - RADIO_POSTE;
+		if(esPosteIzquierda) posicion = RADIO_POSTE;
+		return posicion;
 	}
+	
+	private static double alturaPoste(boolean esPosteArriba) {
+		double medida = -ANCHURA_PORTERIA;
+		if(esPosteArriba) medida = ANCHURA_PORTERIA;
+		return medida;
+	}
+
 
 	@Override
 	public double getVolumen() {
