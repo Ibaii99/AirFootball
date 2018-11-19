c4758e0156244b089d528fad3c032c317db6b346
ibai <ibai@ubuntu>
2018-11-19 11:25:31 +0100

    constructores añadidos a equipo para los test

--- src/test/TestFisicas.java
+++ src/test/TestFisicas.java
@@ -2,19 +2,24 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.Color;
+
 import org.junit.jupiter.api.Test;
 
 import entidades.Equipo;
 import objetos.Pelota;
+import ventanas.ventanaPartido;
 
 class TestFisicas {
 	private Pelota p;
 	private Equipo e1;
 	private Equipo e2;
+	private ventanaPartido v;
 	
 	@Test
 	void testCrearObjetos() {
-		p = new Pelota
+		p = new Pelota(Color.black, "jabulani", 0, 0, 20, 25);
+		e1 = new Equi
 	}
 	
 	@Test
