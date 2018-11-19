f066f85f9ea3752802886407db95f8795a8fc401
ibai <ibai@ubuntu>
2018-11-19 10:55:19 +0100

    classpath con librerias cambiado y añadido al proyecto

---
+++ src/test/TestFisicas.java
@@ -0,0 +1,28 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import entidades.Equipo;
+import objetos.Pelota;
+
+class TestFisicas {
+	private Pelota p;
+	private Equipo e1;
+	private Equipo e2;
+	
+	@Test
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testMovimiento() {
+		
+	}
+	
+
+}
