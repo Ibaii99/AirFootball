26193935ca076263373ad4683c6637b769bc0338
DonBusto <jorge.elbusto@opendeusto.es>
2018-11-29 21:37:49 +0100

    repo local

--- src/ventanas/ventanaPartido.java
+++ src/ventanas/ventanaPartido.java
@@ -193,13 +193,13 @@ public class ventanaPartido extends JFrame {
 		
 		String siglasEqL = eLocal.getSiglas();
 		siglasEqL = siglasEqL.substring(1);
-		JLabel lblNomEqL = new JLabel(siglasEqL);
+		JLabel lblNomEqL = new JLabel("/"+siglasEqL);
 		lblNomEqL.setBounds(53, 5, 262, 36);
 		panel.add(lblNomEqL);
 		
 		String siglasEqV = eVisitante.getSiglas();
 		siglasEqV = siglasEqV.substring(1);
-		JLabel lblNomEqV = new JLabel(siglasEqV);
+		JLabel lblNomEqV = new JLabel("/" + siglasEqV);
 		lblNomEqV.setBounds(464, 5, 262, 36);
 		panel.add(lblNomEqV);
 		getContentPane().setLayout(groupLayout);
