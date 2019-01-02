a2bd233556924ae3309057eae184f41b1b4454cf
DonBusto <jorge.elbusto@opendeusto.es>
2019-01-02 22:50:14 +0100

    Equipos modo creacion sustituyen a normales

--- src/ventanas/VentanaCreacion.java
+++ src/ventanas/VentanaCreacion.java
@@ -228,6 +228,7 @@ public class VentanaCreacion extends JFrame {
 						"iconos/equipos/" + lblLocalizacionIcono.getName(),
 						"iconos/equipos/" + lblLocalizacionIcono.getName(), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 						0);
+				
 				if (!bd.estaJugadorEnBaseDeDatos(tfUsuario.getText(), passwordField.getPassword())) {
 					JOptionPane.showMessageDialog(null, "ESTE JUGADOR NO ESTA REGISTRADO O LA CONTRASEÑA ES ERRONEA",
 							"ERROR", JOptionPane.WARNING_MESSAGE);
@@ -280,8 +281,8 @@ public class VentanaCreacion extends JFrame {
 			String query = "DELETE FROM EQUIPOS" + j.getNombre() + " WHERE NOMBRE = '" + cb.getSelectedItem().toString()
 					+ "';";
 			System.out.println(query);
-			bd.getCon().createStatement().executeUpdate(query); //executeUpdate para inserts y deletes y executeQuery para selects
-
+			bd.getCon().createStatement().executeUpdate(query); // executeUpdate para inserts y deletes y executeQuery
+																// para selects
 			bd.close();
 		} catch (Exception e3) {
 			e3.printStackTrace();
@@ -297,7 +298,7 @@ public class VentanaCreacion extends JFrame {
 		}
 		String comando = "INSERT INTO Equipos" + j.getNombre()
 				+ " ('fk_CodLiga', 'fk_Nombre_Jugador','Siglas', 'Nombre', 'Puntos', 'Goles Encajados Totales', 'Goles Encajados Local', 'Goles Encajados Visitante', 'Goles A Favor Totales', 'Goles A Favor Local', 'Goles A Favor Visitante', 'Derrotas Totales', 'Derrotas Local', 'Derrotas Visitante', 'Victorias Totales', 'Victorias Local', 'Victorias Visitante', 'Empates Totales', 'Empates Local', 'Empates Visitante', 'Color', 'Icono')";
-		comando += " VALUES ('" + 0 + "','" + j.getNombre() + "','" + e.getSiglas() + "','" + e.getNombre() // mira aqui
+		comando += " VALUES ('" + 1 + "','" + j.getNombre() + "','" + e.getSiglas() + "','" + e.getNombre() // mira aqui
 																											// bien lo
 																											// de
 																											// CodLiga
@@ -310,7 +311,7 @@ public class VentanaCreacion extends JFrame {
 				+ e.getDerrotasLocal() + "','" + e.getDerrotasVisitante() + "','" + e.getVictoriasTotales() + "','"
 				+ e.getVictoriasLocal() + "','" + e.getVictoriasVisitante() + "','" + e.getEmpatesTotales() + "','"
 				+ e.getEmpatesLocal() + "','" + e.getEmpatesVisitante() + "','" + e.getBolaEquipo().getColor().getRGB()
-				+ "','" + e.getBolaEquipo().getRutaImagen() + "')";
+				+ "','" + e.getImagen() + "')";
 		System.out.println(comando);
 		ResultSet rs = bd.getCon().createStatement().executeQuery(comando);
 		bd.close();
